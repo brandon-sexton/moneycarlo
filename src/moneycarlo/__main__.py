479bb0567f679f5d93012ea7f0e8ebcebcd1d961
Sexton <brandon.sexton.1@outlook.com>
2023-05-07 17:31:23 +0000

    changed main method to print prediction bounds

--- src/moneycarlo/__main__.py
+++ src/moneycarlo/__main__.py
@@ -1,6 +1,8 @@
 import sys
+from statistics import median
 
 from moneycarlo.contracts import RevenuePlan
 
 if __name__ == "__main__":
-    print(RevenuePlan.from_csv(sys.argv[1]).predict_revenue(int(sys.argv[2])))
+    pred = RevenuePlan.from_csv(sys.argv[1]).predict_revenue(int(sys.argv[2]))
+    print(min(pred), median(pred), max(pred))
