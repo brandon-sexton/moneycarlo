5621c83644ca7916d20dabdd3245ae4bff84e2ec
Sexton <brandon.sexton.1@outlook.com>
2023-05-07 18:30:25 +0000

    added print formats

--- src/moneycarlo/__main__.py
+++ src/moneycarlo/__main__.py
@@ -1,8 +1,17 @@
 import sys
-from statistics import median
+from statistics import mode
 
 from moneycarlo.contracts import RevenuePlan
 
 if __name__ == "__main__":
-    pred = RevenuePlan.from_csv(sys.argv[1]).predict_revenue(int(sys.argv[2]))
-    print(min(pred), median(pred), max(pred))
+    its = int(sys.argv[2])
+    pred = RevenuePlan.from_csv(sys.argv[1]).predict_revenue(its)
+    low = min(pred)
+    high = max(pred)
+    best = mode(pred)
+    plow = pred.count(low) / its * 100
+    phigh = pred.count(high) / its * 100
+    pbest = pred.count(best) / its * 100
+    print("Low:    ${:12.2f} with {:6.2f}% occurrence".format(low, plow))
+    print("High:   ${:12.2f} with {:6.2f}% occurrence".format(high, phigh))
+    print("Mode:   ${:12.2f} with {:6.2f}% occurrence".format(best, pbest))
